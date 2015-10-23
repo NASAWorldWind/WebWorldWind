28d0d402b97c2dacb52cbce06ce5b16712fc126c
Jakub Balhar <jakub@balhar.net>
2015-10-23 08:38:42 +0200

    Added polyfill for ES6 Promises for browser not supporting it.

--- src/util/Promise.js
+++ src/util/Promise.js
@@ -2,47 +2,11 @@
  * Copyright (C) 2014 United States Government as represented by the Administrator of the
  * National Aeronautics and Space Administration. All Rights Reserved.
  */
-define([], function () {
+define(['./es6-promise'], function (ES6Promise) {
     "use strict";
-    if(Promise) {
+    if (Promise) {
         return Promise;
+    } else {
+        return ES6Promise.Promise;
     }
-
-    // Promise implementation equal to ECMA Script 6 standard.
-    // Skeleton for now.
-    var PromiseLegacy = function() {
-        this.all = function(iterable) {
-
-        };
-
-        this.race = function(iterable) {
-
-        };
-
-        this.reject = function(reason) {
-
-        };
-
-        this.resolve = function(value) {
-
-        };
-
-        this.length = 1;
-    };
-
-    Object.defineProperties(PromiseLegacy.prototype, {
-        constructor: {
-            get: function(){
-                return PromiseLegacy;
-            }
-        }
-    });
-
-    PromiseLegacy.prototype.catch = function() {
-
-    };
-
-    PromiseLegacy.prototype.then = function() {
-
-    };
 });
