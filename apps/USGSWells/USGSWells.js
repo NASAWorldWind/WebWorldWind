49a80a2db79a2bf6d8c5f6ac37adaf8f771e8459
pdavidc <paul@paulcollinssoftware.com>
2015-12-03 17:07:13 -0800

    Added tool tip text to the USGS wells app.

--- apps/USGSWells/USGSWells.js
+++ apps/USGSWells/USGSWells.js
@@ -37,8 +37,8 @@ define(['../../src/WorldWind',
 
             // Configure the USGS installed well layers.
             var layer = new WorldWind.RenderableLayer("USGS Wells");
-            this.addWellShape(33.098751, -117.01714, 270, 30, layer);
-            this.addWellShape(33.087345, -116.974756, 340, 30, layer);
+            this.addWellShape(33.098751, -117.01714, 270, 30, "Cloverdale\n330555117010101-3\n12S/1W-30J3-5", layer);
+            this.addWellShape(33.087345, -116.974756, 340, 30, "Santa Ysabel\n330514116582901-3\n12S/1W-34L2-5", layer);
             this.wwd.addLayer(layer);
 
             // Enable sub-surface rendering for the World Window.
@@ -65,7 +65,7 @@ define(['../../src/WorldWind',
             this.terrainOpacityController = new TerrainOpacityController(this.wwd);
         };
 
-        USGSWells.prototype.addWellShape = function (latitude, longitude, depthFeet, radiusFeet, layer) {
+        USGSWells.prototype.addWellShape = function (latitude, longitude, depthFeet, radiusFeet, displayText, layer) {
             var boundary = [],
                 feetToMeters = 0.3048,
                 center = new WorldWind.Location(latitude, longitude),
@@ -81,17 +81,63 @@ define(['../../src/WorldWind',
             }
 
             var polygon = new WorldWind.Polygon([boundary]);
+            polygon.pickDelegate = displayText;
             polygon.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
             polygon.extrude = true;
             polygon.attributes.interiorColor = new WorldWind.Color(0.22, 0.65, 0.87, 1.0);
-            polygon.attributes.outlineColor = new WorldWind.Color(0, 0, 1, 1);
+            polygon.attributes.outlineColor = WorldWind.Color.BLUE;
             polygon.attributes.applyLighting = true;
             polygon.highlightAttributes = new WorldWind.ShapeAttributes(polygon.attributes);
             polygon.highlightAttributes.interiorColor = WorldWind.Color.WHITE;
             layer.addRenderable(polygon);
+
+            var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 0));
+            placemark.pickDelegate = displayText;
+            placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+            placemark.eyeDistanceScaling = true;
+            placemark.attributes.imageSource = WorldWind.configuration.baseUrl + "images/white-dot.png";
+            placemark.attributes.imageColor = WorldWind.Color.RED;
+            placemark.attributes.imageScale = 0.15;
+            layer.addRenderable(placemark);
         };
 
         USGSWells.prototype.setupPicking = function () {
+            this.screenText = new WorldWind.ScreenText(new WorldWind.Offset(WorldWind.OFFSET_PIXELS, 0, WorldWind.OFFSET_PIXELS, 0), " ");
+            this.screenText.attributes = new WorldWind.TextAttributes();
+            this.screenText.attributes.color = WorldWind.Color.YELLOW;
+            this.screenText.attributes.offset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0, WorldWind.OFFSET_FRACTION, 0);
+
+            this.textLayer = new WorldWind.RenderableLayer();
+            this.textLayer.hide = true;
+            this.textLayer.enabled = false;
+            this.textLayer.addRenderable(this.screenText);
+            this.wwd.addLayer(this.textLayer);
+
+            var handlePick = (function (o) {
+                var pickPoint = this.wwd.canvasCoordinates(o.clientX, o.clientY);
+
+                this.textLayer.enabled = false;
+                this.wwd.redraw();
+
+                var pickList = this.wwd.pick(pickPoint);
+                if (pickList.objects.length > 0) {
+                    for (var p = 0; p < pickList.objects.length; p++) {
+                        var pickedObject = pickList.objects[p];
+                        if (typeof pickedObject.userObject === "string") {
+                            this.screenText.screenOffset.x = pickPoint[0];
+                            this.screenText.screenOffset.y = this.wwd.viewport.width - pickPoint[1];
+                            this.screenText.text = pickedObject.userObject;
+                            this.textLayer.enabled = true;
+                        }
+                    }
+                }
+            }).bind(this);
+
+            // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
+            this.wwd.addEventListener("mousemove", handlePick);
+
+            // Listen for taps on mobile devices and highlight the placemarks that the user taps.
+            var tapRecognizer = new WorldWind.TapRecognizer(this.wwd, handlePick);
         };
 
         return USGSWells;
