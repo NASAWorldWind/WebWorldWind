3f0bdcb97917ff2e1d858b72f781128aa0f58a54
strikerM <k_lite_07@yahoo.com>
2016-01-20 11:58:19 +0200

    fix surface shapes transparency

--- src/shapes/SurfaceShape.js
+++ src/shapes/SurfaceShape.js
@@ -840,7 +840,7 @@ define([
 
             // Fill the interior of the shape.
             if (!this._isInteriorInhibited && attributes.drawInterior) {
-                ctx2D.fillStyle = isPicking ? this.pickColor.toHexString(false) : attributes.interiorColor.toHexString(false);
+                ctx2D.fillStyle = isPicking ? this.pickColor.toRGBAString() : attributes.interiorColor.toRGBAString();
 
                 for (idx = 0, len = this._interiorGeometry.length; idx < len; idx += 1) {
                     idxPath = 0;
@@ -868,7 +868,7 @@ define([
             // Draw the outline of the shape.
             if (attributes.drawOutline && attributes.outlineWidth > 0) {
                 ctx2D.lineWidth = 4 * attributes.outlineWidth;
-                ctx2D.strokeStyle = isPicking ? this.pickColor.toHexString(false) : attributes.outlineColor.toHexString(false);
+                ctx2D.strokeStyle = isPicking ? this.pickColor.toRGBAString() : attributes.outlineColor.toRGBAString();
 
                 var pattern = this._attributes.outlineStipplePattern,
                     factor = this._attributes.outlineStippleFactor;
