fc0a58483ebf9e78718ed990888991ff0940c881
Jakub Balhar <jakub@balhar.net>
2016-12-06 10:47:23 +0100

    computeViewTilt works correctly test.

--- test/globe/Globe.test.js
+++ test/globe/Globe.test.js
@@ -36,5 +36,18 @@ define([
                 expect(viewHeading).toBe(-144.74739580118177);
             });
         });
+
+        describe('#computeViewTilt', function(){
+            it('returns correct tilt when it is applied', function(){
+                var source = new Matrix(
+                    -0.4878596591387329, 0.4906549897935131, -0.7219768929652575, -4610466.9131683465,
+                    2.7755575615628914E-17, 0.8270805742745618, 0.5620833778521306, 3565379.0227454384,
+                    0.8729220772698095, 0.274217805126488, -0.4034992470458552, -2576702.8642047923,
+                    0.0, 0.0, 0.0, 1.0
+                );
+                var viewTilt = globe.computeViewTilt(source);
+                expect(viewTilt).toBe(113.79711637384179);
+            });
+        });
     });
 });
