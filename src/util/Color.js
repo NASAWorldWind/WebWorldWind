1b8900e48ecefa9ab1fcaf9b86b73f967b04c75e
Jakub Balhar <jakub@balhar.net>
2017-05-22 10:02:54 +0200

    Correctly compute color from KML hex.

--- src/util/Color.js
+++ src/util/Color.js
@@ -221,8 +221,11 @@ define([
         };
 
         Color.colorFromKmlHex = function(color) {
-            color = color.split("").reverse().join("");
-            return Color.colorFromHex(color);
+            var alpha = parseInt(color.substring(0,2), 16);
+            var blue = parseInt(color.substring(2,4), 16);
+            var green = parseInt(color.substring(4,6), 16);
+            var red = parseInt(color.substring(6, 8), 16);
+            return Color.colorFromBytes(red, green, blue, alpha);
         };
 
         /**
