8eb69e7f6a11b9bf075ce1c4b36d882d7720b55e
Miguel Del Castillo <migueldelcastillo@live.com>
2017-08-09 09:39:58 -0500

    Removed CRLF at the end of .npmignore

--- tools/npmEntryPoint.js
+++ tools/npmEntryPoint.js
@@ -1,5 +1,5 @@
 // This file enables the consumption of the production build of the library in a CommonJS interface
-// for the purposes of publishing it as a package in Node Package Manager (npm). Grunt copies this
-// file to the root folder of the project and
-// Currently (August 2017), scoped packages with an entry point other than "index.js" do not work.
+// for the purposes of publishing it as a package in Node Package Manager (npm). This is used as a
+// workaround since currently (August 2017), scoped packages with an entry point other than
+// "index.js" do not work.
 module.exports = require('./worldwind.min.js');
