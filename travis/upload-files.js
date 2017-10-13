bf634512d872af8e44725ff3155beed3ad362241
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2017-10-13 13:48:50 -0500

    Refactor function naming and structure

--- travis/upload-files.js
+++ travis/upload-files.js
@@ -1,33 +1,71 @@
 var http = require("https");
 var fs = require("fs");
 
-var minifiedFileOptions = {
-    "method": "PUT",
-    "hostname": "files.worldwind.arc.nasa.gov",
-    "port": null,
-    "path": "/artifactory/generic-local/webworldwind/worldwind/worldwind-" + process.env.TRAVIS_TAG.slice(1) + ".min.js",
-    "headers": {
-        "authorization": process.env.FILES_API_KEY,
-        "cache-control": "no-cache"
-    }
-};
+var version;
+if (!process.env.TRAVIS_TAG || !process.env.TRAVIS_TAG.startsWith("v")) {
+    console.error("invalid version tag");
+    process.exit(101);
+} else {
+    version = process.env.TRAVIS_TAG.slice(1);
+}
 
-var debugFileOptions = {
-    "method": "PUT",
-    "hostname": "files.worldwind.arc.nasa.gov",
-    "port": null,
-    "path": "/artifactory/generic-local/webworldwind/worldwind/worldwind-" + process.env.TRAVIS_TAG.slice(1) + ".js",
-    "headers": {
-        "authorization": process.env.FILES_API_KEY,
-        "cache-control": "no-cache"
-    }
+if (!process.env.FILES_API_KEY) {
+    console.error("missing file server api key");
+    process.exit(102);
+}
+
+/**
+ * Sends a file to the server
+ * @param filename the local filename of the file to send
+ * @param options an object defining the method, hostname, port, path, and headers
+ */
+var sendFileToServer = function (filename, options) {
+
+    var req = http.request(options, onRequest);
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        req.write(data);
+        req.end();
+    });
 };
 
 /**
- * A simple request callback which concatenates the response and logs to the console.
+ * Sends all of the files from a directory to a server. Recursively traverses the provided starting directory.
+ * @param filename the local filename of the directory to start
+ */
+var sendDirectoryToServer = function (filename) {
+    fs.readdir(filename, onReadDirectory(filename).processFiles);
+};
+
+/**
+ * Creates the boilerplate options given the desired upload path
+ * @param filename
+ * @returns {{method: string, hostname: string, port: null, path: string, headers: {authorization: *, cache-control: string}}}
+ */
+var createRequestOptions = function (filename) {
+
+    var options = {
+        "method": "PUT",
+        "hostname": "files.worldwind.arc.nasa.gov",
+        "port": null,
+        "path": "/artifactory/generic-local/webworldwind/" + filename,
+        "headers": {
+            "authorization": process.env.FILES_API_KEY,
+            "cache-control": "no-cache"
+        }
+    };
+
+    return options;
+};
+
+/**
+ * The callback for the request method. Processes the response from the server and displays the results.
  * @param res
  */
-var requestCallback = function (res) {
+var onRequest = function (res) {
     var chunks = [];
 
     res.on("data", function (chunk) {
@@ -41,22 +79,55 @@ var requestCallback = function (res) {
 };
 
 /**
- * Reads a file from disk and submits it content hosting service.
- * @param filename
- * @param options
+ * The callback for reading a directory. Traverses the files provided in the callback and checks if the path is a file
+ * or directory.
+ * @param baseDirectory
+ * @returns {{processFiles: processFiles}}
  */
-var pushRequestToServer = function (filename, options) {
-    var req = http.request(options, requestCallback);
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            console.log(err);
-        }
+var onReadDirectory = function (baseDirectory) {
+    var directory = baseDirectory;
+    return {
+        processFiles: function (err, files) {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-        req.write(data);
-        req.end();
-    });
+            for (var i = 0, len = files.length; i < len; i++) {
+                var fullFilename = directory + "/" + files[i];
+                fs.stat(fullFilename, onFileStats(fullFilename, "assets/" + version).processFile);
+            }
+        }
+    }
 };
 
-// Submitting files to the content hosting service
-pushRequestToServer("worldwind.min.js", minifiedFileOptions);
-pushRequestToServer("worldwind.js", debugFileOptions);
+/**
+ * The callback for determining if a path refers to a file or directory. Process the path depending on type.
+ * @param filename
+ * @returns {{processFile: processFile}}
+ */
+var onFileStats = function (filename, uploadPath) {
+    var fn = filename;
+    var upload = uploadPath;
+    return {
+        processFile:  function (err, stat) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            if (stat.isDirectory()) {
+                sendDirectoryToServer(fn);
+            } else {
+                // This assumes the file to be sent is part of the image assets directory
+                sendFileToServer(fn, createRequestOptions(upload + "/" + fn));
+            }
+        }
+    }
+};
+
+// Upload desired assets
+sendFileToServer("worldwind.min.js", createRequestOptions("worldwind/worldwind-" + version + ".min.js"));
+sendFileToServer("worldwind.js", createRequestOptions("worldwind/worldwind-" + version + ".js"));
+sendFileToServer("images.zip", createRequestOptions("assets/" + version + "/images.zip"));
+sendDirectoryToServer("./images");
