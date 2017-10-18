a49178f75827406a40b97b3317c0132c748fd7b3
Miguel Del Castillo <migueldelcastillo@live.com>
2017-10-18 09:36:25 -0700

    Test to update state of both a placemark and a SurfaceShape ellipse  - Placemark's position updates correctly, the ellipse heading    is not being modified

--- examples/SurfaceShapesUpdating.js
+++ examples/SurfaceShapesUpdating.js
@@ -36,19 +36,43 @@ requirejs(['../src/WorldWind',
             wwd.addLayer(layers[l].layer);
         }
 
-        // Create a layer to hold the surface shapes.
-        var shapesLayer = new WorldWind.RenderableLayer("Surface Shapes");
+        // Create placemark layer
+        var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+
+        // Set up attibutes for the placemark
+        var pinLibrary = WorldWind.configuration.baseUrl + "images/pushpins/",
+            placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
+            latitude = 47.684444,
+            longitude = -121.129722;
+
+        placemarkAttributes.imageScale = 1;
+        placemarkAttributes.imageOffset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.3,
+            WorldWind.OFFSET_FRACTION, 0.0);
+        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
+        placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 1.0);
+        placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+        placemarkAttributes.drawLeaderLine = true;
+        placemarkAttributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
+        placemarkAttributes.imageSource = pinLibrary + "plain-red.png";
+
+        // Define placemark position. This position will update every second.
+        var placemarkPosition = new WorldWind.Position(latitude, longitude, 1e2);
+
+        var placemark = new WorldWind.Placemark(placemarkPosition);
+        placemark.attributes = placemarkAttributes;
+        placemark.label = "Placemark ";
+        placemarkLayer.addRenderable(placemark);
+
+        wwd.addLayer(placemarkLayer);
+
+        // Create a layer to hold a surface shape (in this case, an ellipse)
+        var shapesLayer = new WorldWind.RenderableLayer("Ellipse");
         wwd.addLayer(shapesLayer);
 
-        // Create a simple surface polygon, a triangle.
-        var boundary = [];
-        boundary.push(new WorldWind.Location(40, -100));
-        boundary.push(new WorldWind.Location(45, -110));
-        boundary.push(new WorldWind.Location(40, -120));
-
-        // Create and set attributes for it. The shapes below except the surface polyline use this same attributes
-        // object. Real apps typically create new attributes objects for each shape unless they know the attributes
-        // can be shared among shapes.
+        // Set attributes for the shape
         var attributes = new WorldWind.ShapeAttributes(null);
         attributes.outlineColor = WorldWind.Color.BLUE;
         attributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
@@ -56,58 +80,10 @@ requirejs(['../src/WorldWind',
         var highlightAttributes = new WorldWind.ShapeAttributes(attributes);
         highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 1);
 
-        var shape = new WorldWind.SurfacePolygon(boundary, attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a polygon with a hole in it.
-        var boundaries = [];
-        boundaries[0] = []; // outer boundary
-        boundaries[0].push(new WorldWind.Location(40, -70));
-        boundaries[0].push(new WorldWind.Location(45, -80));
-        boundaries[0].push(new WorldWind.Location(40, -90));
-        boundaries[1] = []; // inner boundary
-        boundaries[1].push(new WorldWind.Location(41, -73));
-        boundaries[1].push(new WorldWind.Location(44, -80));
-        boundaries[1].push(new WorldWind.Location(41, -87));
-
-        shape = new WorldWind.SurfacePolygon(boundaries, attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a surface circle with a radius of 200 km.
-        shape = new WorldWind.SurfaceCircle(new WorldWind.Location(35, -120), 200e3, attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a surface ellipse with a minor radius of 200 km, a major radius of 300 km and a heading of 45 degrees.
-        shape = new WorldWind.SurfaceEllipse(new WorldWind.Location(35, -110), 300e3, 200e3, 45, attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a surface rectangle with a width of 200 km, a height of 300 km and a heading of -45 degrees.
-        shape = new WorldWind.SurfaceRectangle(new WorldWind.Location(35, -100), 200e3, 300e3, -45, attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a surface sector.
-        shape = new WorldWind.SurfaceSector(new WorldWind.Sector(33, 37, -95, -90), attributes);
-        shape.highlightAttributes = highlightAttributes;
-        shapesLayer.addRenderable(shape);
-
-        // Create a surface polyline. Use different attributes then for the filled shapes.
-        boundary = [];
-        boundary.push(new WorldWind.Location(33, -75));
-        boundary.push(new WorldWind.Location(37, -80));
-        boundary.push(new WorldWind.Location(33, -85));
-
-        attributes = new WorldWind.ShapeAttributes(null);
-        attributes.outlineColor = new WorldWind.Color(0, 1, 1, 0.5);
-
-        highlightAttributes = new WorldWind.ShapeAttributes(attributes);
-        highlightAttributes.outlineColor = new WorldWind.Color(1, 1, 1, 1);
-
-        shape = new WorldWind.SurfacePolyline(boundary, attributes);
+        // Create a surface ellipse with a minor radius of 200 km, a major radius of 300 km.
+        // This example tests changing the heading every second
+        var currentHeading = 0;
+        var shape = new WorldWind.SurfaceEllipse(new WorldWind.Location(35, -110), 900e3, 600e3, currentHeading, attributes);
         shape.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(shape);
 
@@ -116,5 +92,17 @@ requirejs(['../src/WorldWind',
 
         // Now set up to handle highlighting.
         var highlightController = new WorldWind.HighlightController(wwd);
+
+        // Update SurfaceShape an placemark states
+        var follow = false;
+        window.setInterval(function() {
+            // Update placemark position
+            placemarkPosition.latitude += 10;
+            placemarkPosition.longitude += 10;
+
+            // Update ellipse heading angle (as of now, this fails)
+            currentHeading += 10;
+            wwd.redraw();
+        }, 1000);
     }
 );
