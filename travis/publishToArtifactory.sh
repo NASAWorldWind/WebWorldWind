517f5443c4d7aec631e98a107d3c2743039bf32b
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2017-10-24 06:17:55 -0500

    Rename scripts

--- travis/publishToArtifactory.sh
+++ travis/publishToArtifactory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+a
 mkdir assetsToPublish
 npm pack
 tar -xvf nasaworldwind-worldwind-*.tgz -C ./assetsToPublish --strip-components 1
-node travis/publishToArtifactory $ARTIFACTORY_API_KEY ${TRAVIS_TAG#"v"} assetsToPublish/
+node travis/publishFolderToArtifactory $ARTIFACTORY_API_KEY ${TRAVIS_TAG#"v"} assetsToPublish/
