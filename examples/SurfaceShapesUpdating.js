2c6725015a21e09b184e205d9459b9504e41b371
Miguel Del Castillo <migueldelcastillo@live.com>
2017-10-24 13:21:10 -0500

    Added all SurfaceShapes to example. Simplified updating.

--- examples/SurfaceShapesUpdating.js
+++ examples/SurfaceShapesUpdating.js
@@ -3,7 +3,7 @@
  * National Aeronautics and Space Administration. All Rights Reserved.
  */
 /**
- * Illustrates how to display SurfaceShapes.
+ * Illustrates how to update SurfaceShapes.
  *
  * @version $Id: SurfaceShapes.js 3320 2015-07-15 20:53:05Z dcollins $
  */
@@ -35,103 +35,80 @@ requirejs(['../src/WorldWind',
             wwd.addLayer(layers[l].layer);
         }
 
-        // Create placemark layer
-        var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-
-        // Set up attributes for the placemark
-        var pinLibrary = WorldWind.configuration.baseUrl + "images/pushpins/",
-            placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
-            latitude = 47.684444,
-            longitude = -121.129722;
-
-        placemarkAttributes.imageScale = 1;
-        placemarkAttributes.imageOffset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.3,
-            WorldWind.OFFSET_FRACTION, 0.0);
-        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
-        placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 1.0);
-        placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-        placemarkAttributes.drawLeaderLine = true;
-        placemarkAttributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
-        placemarkAttributes.imageSource = pinLibrary + "plain-red.png";
-
-        // Define placemark position. This position will update every second.
-        var placemarkPosition = new WorldWind.Position(latitude, longitude, 1e2);
-
-        var placemark = new WorldWind.Placemark(placemarkPosition);
-        placemark.attributes = placemarkAttributes;
-        placemark.label = "Placemark ";
-        placemarkLayer.addRenderable(placemark);
-
-        wwd.addLayer(placemarkLayer);
-
         // Create a layer to hold surface shapes
         var shapesLayer = new WorldWind.RenderableLayer("Surface Shapes");
         wwd.addLayer(shapesLayer);
 
-        // Set attributes for the shape
+        // Set attributes for the shapes
         var attributes = new WorldWind.ShapeAttributes(null);
+        attributes.outlineWidth = 2;
         attributes.outlineColor = WorldWind.Color.BLUE;
         attributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
 
-        // Create a surface ellipse with a minor radius of 200 km, a major radius of 300 km and heading of 0 deg.
-        // Heading should change every second
-        var ellipse = new WorldWind.SurfaceEllipse(new WorldWind.Location(35, -110), 900e3, 600e3, 0, attributes);
+        // Create different types of SurfaceShapes and add them to the Surface Shapes layer
 
-        // Add ellipse to the shapes layer
+        // Surface circle
+        var circle = new WorldWind.SurfaceCircle(new WorldWind.Location(35, -120), 200e3, attributes);
+        shapesLayer.addRenderable(circle);
+
+        // Surface ellipse
+        var ellipse = new WorldWind.SurfaceEllipse(new WorldWind.Location(35, -110), 300e3, 200e3, 45, attributes);
         shapesLayer.addRenderable(ellipse);
 
-        // Create a surface rectangle with a width of 200 km, a height of 300 km and a heading of -45 degrees.
+        // Surface rectangle
         var rectangle = new WorldWind.SurfaceRectangle(new WorldWind.Location(35, -100), 200e3, 300e3, -45, attributes);
-
         shapesLayer.addRenderable(rectangle);
 
-        // Create a surface sector
+        // Surface Sector
         var sector = new WorldWind.SurfaceSector(new WorldWind.Sector(33, 37, -95, -90), attributes);
         shapesLayer.addRenderable(sector);
 
-        // Create a simple surface polygon, a triangle.
-        var boundary = [];
-        boundary.push(new WorldWind.Location(40, -100));
-        boundary.push(new WorldWind.Location(45, -110));
-        boundary.push(new WorldWind.Location(40, -120));
-        var triangle = new WorldWind.SurfacePolygon(boundary, attributes);
-        shapesLayer.addRenderable(triangle);
+        // Surface polygon
+        var polygonBoundary = [];
+        polygonBoundary = [];
+        polygonBoundary.push(new WorldWind.Location(40, -105));
+        polygonBoundary.push(new WorldWind.Location(45, -110));
+        polygonBoundary.push(new WorldWind.Location(40, -115));
+        polygonBoundary.tick = true;
+
+        var polygon = new WorldWind.SurfacePolygon(polygonBoundary, attributes);
+        shapesLayer.addRenderable(polygon);
+
+        // Surface polyline
+        var polylineBoundary = [];
+        polylineBoundary.push(new WorldWind.Location(40, -90));
+        polylineBoundary.push(new WorldWind.Location(45, -95));
+        polylineBoundary.push(new WorldWind.Location(40, -100));
+        polylineBoundary.tick = true;
+
+        var polyline = new WorldWind.SurfacePolyline(polylineBoundary, attributes);
+        shapesLayer.addRenderable(polyline);
 
         // Create a layer manager for controlling layer visibility.
         var layerManger = new LayerManager(wwd);
 
-        console.log(boundary);
-        console.log(triangle._boundaries);
 
-        // Update SurfaceShape heading an placemark position with a timer
+        // Update SurfaceShapes according to a timer
         window.setInterval(function() {
-            // Update placemark position
-            placemarkPosition.latitude += 5;
-            placemarkPosition.longitude += 5;
-
-            // Update ellipse heading angle
+            // Rotate ellipse 10 degrees clockwise
             ellipse.heading += 10;
-            //ellipse.isPrepared = false;
-            //ellipse._boundaries = null;
 
-            // Update the rectangle heading angle
-            rectangle.heading -= 45;
-            //rectangle.isPrepared = false;
-            //rectangle._boundaries = null;
+            // Rotate rectangle 30 degrees counterclockwise
+            rectangle.heading -= 30;
 
-            // Update sector max lat and long
-            sector.maxLatitude += 5;
-            sector.maxLongitude += 5;
-            sector.isPrepared = false;
-            sector._boundaries = null;
+            // Update circle radius size
+            circle.radius == 200e3 ? circle.radius += 100e3 : circle.radius = 200e3;
 
-            // Update triangle polygon latitude
-            increaseBoundariesLatitude(triangle);
-            triangle.isPrepared = false;
-            sector._boundaries = null;
+            // Enable and disable polygon
+            polygon.enabled ? polygon.enabled = false : polygon.enabled = true;
+
+            // Enable and disable sector
+            sector.enabled ? sector.enabled = false : sector.enabled = true;
+
+            // Increase and decrease polyline polygon size (probably not the correct way to do it)
+            // polyline.boundaries[0].longitude === -90 ? polyline.boundaries[0].longitude = -92.5 : polyline.boundaries[0].longitude = -90;
+            // polyline.boundaries[1].latitude === 45 ? polyline.boundaries[1].latitude = 42.5 : polyline.boundaries[1].latitude = 45;
+            // polyline.boundaries[2].longitude === -100 ? polyline.boundaries[2].longitude = -97.5 : polyline.boundaries[2].longitude = -100;
 
             wwd.redraw();
         }, 1000);
