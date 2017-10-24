807898f41464878dbe862c2f10c43b951ee44990
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2017-10-24 09:31:43 -0500

    Clean up command options and path formats

--- travis/publishToArtifactory.sh
+++ travis/publishToArtifactory.sh
@@ -2,5 +2,5 @@
 
 mkdir assetsToPublish
 npm pack
-tar -xvf nasaworldwind-worldwind-*.tgz -C ./assetsToPublish --strip-components 1
-node travis/publishFolderToArtifactory $ARTIFACTORY_API_KEY ./assetsToPublish/ /artifactory/web/${TRAVIS_TAG#"v"}
+tar -xf nasaworldwind-worldwind-*.tgz -C ./assetsToPublish --strip-components 1
+node travis/publishFolderToArtifactory $ARTIFACTORY_API_KEY ./assetsToPublish /artifactory/web/${TRAVIS_TAG#"v"}
