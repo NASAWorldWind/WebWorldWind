698f304010dcffe2d62c522712794653a9644f8a
Miguel Del Castillo <migueldelcastillo@live.com>
2017-10-27 08:36:19 -0600

    Added highlight controller to SurfaceShapesUpdating example  - Corrected typos and polished comments

--- examples/SurfaceShapesUpdating.js
+++ examples/SurfaceShapesUpdating.js
@@ -39,24 +39,30 @@ requirejs(['../src/WorldWind',
         var shapesLayer = new WorldWind.RenderableLayer("Surface Shapes");
         wwd.addLayer(shapesLayer);
 
-        // Set attributes for the shapes
+        // Create and set attributes that will be shared with all the shapes.
         var attributes = new WorldWind.ShapeAttributes(null);
         attributes.outlineWidth = 2;
         attributes.outlineColor = WorldWind.Color.BLUE;
         attributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
 
+        var highlightAttributes = new WorldWind.ShapeAttributes(attributes);
+        highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 1);
+
         // Create different types of SurfaceShapes and add them to the Surface Shapes layer
 
         // Surface circle
         var circle = new WorldWind.SurfaceCircle(new WorldWind.Location(35, -120), 200e3, attributes);
+        circle.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(circle);
 
         // Surface ellipse
         var ellipse = new WorldWind.SurfaceEllipse(new WorldWind.Location(35, -110), 300e3, 200e3, 45, attributes);
+        ellipse.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(ellipse);
 
         // Surface rectangle
         var rectangle = new WorldWind.SurfaceRectangle(new WorldWind.Location(35, -100), 200e3, 300e3, -45, attributes);
+        rectangle.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(rectangle);
 
         // Surface Sector
@@ -66,11 +72,11 @@ requirejs(['../src/WorldWind',
         rotatingSectors.push(new WorldWind.Sector(31, 35, -97, -92));
         rotatingSectors.push(new WorldWind.Sector(31, 35, -95, -90));
         var sector = new WorldWind.SurfaceSector(rotatingSectors[0], attributes);
+        sector.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(sector);
 
         // Surface polygon
         var polygonBoundary = [];
-        polygonBoundary = [];
         polygonBoundary.push(new WorldWind.Location(40, -105));
         polygonBoundary.push(new WorldWind.Location(45, -110));
         polygonBoundary.push(new WorldWind.Location(40, -115));
@@ -78,6 +84,7 @@ requirejs(['../src/WorldWind',
         var polygon = new WorldWind.SurfacePolygon(polygonBoundary, attributes);
         // Append property to count the times the polygon has shifted eastward or westward
         polygon.shiftCounter = 0;
+        polygon.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(polygon);
 
         // Surface polyline
@@ -90,10 +97,18 @@ requirejs(['../src/WorldWind',
         // Append property to count the times the polyline has shifted eastward or westward
         polyline.shiftCounter = 0;
 
+        // Since polyline doesn't have an interior, we will set different highlight attributes for it.
+        highlightAttributes = new WorldWind.ShapeAttributes(attributes);
+        highlightAttributes.outlineColor = new WorldWind.Color(0, 1, 1, 0.5);
+
+        polyline.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(polyline);
 
         // Create a layer manager for controlling layer visibility.
         var layerManger = new LayerManager(wwd);
+
+        // Create a highlight controller for highlighting.
+        var highlightController = new WorldWind.HighlightController(wwd);
 
         // Update SurfaceShapes according to a timer
         window.setInterval(function() {
@@ -104,10 +119,10 @@ requirejs(['../src/WorldWind',
             rectangle.heading -= 30;
 
             // Update circle radius size
-            circle.radius == 200e3 ? circle.radius += 100e3 : circle.radius = 200e3;
+            circle.radius === 200e3 ? circle.radius += 100e3 : circle.radius = 200e3;
 
+            // Shift polygon and polyline left and right
             polygon.boundaries = shiftBoundaries(polygon);
-
             polyline.boundaries = shiftBoundaries(polyline);
 
             // sector.sector = moveSector();
@@ -116,7 +131,6 @@ requirejs(['../src/WorldWind',
         }, 1000);
 
         function shiftBoundaries(shape){
-            console.log()
             if (shape.shiftCounter < 2){
                 shape.boundaries = shiftBoundariesWestward(shape.boundaries);
                 shape.shiftCounter += 2;
@@ -130,21 +144,21 @@ requirejs(['../src/WorldWind',
         }
 
         function shiftBoundariesWestward(boundaries){
-            for(var i = 0; i < boundaries.length; i++){
+            for (var i = 0; i < boundaries.length; i++) {
                 boundaries[i].longitude -= 1;
             }
             return boundaries;
         }
 
         function shiftBoundariesEastward(boundaries){
-            for(var i = 0; i < boundaries.length; i++){
+            for (var i = 0; i < boundaries.length; i++) {
                 boundaries[i].longitude += 1;
             }
             return boundaries;
         }
 
         function moveSector(){
-            for (var i = 0; i < rotatingSectors.length; i++){
+            for (var i = 0; i < rotatingSectors.length; i++) {
                 return rotatingSectors[i];
             }
         }
