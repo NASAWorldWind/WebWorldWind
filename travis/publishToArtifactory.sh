7333d9bb54293eba496ee357bf81827a9ea5d2e1
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2017-11-06 22:47:56 -0600

    Fix Artifactory deployment path

--- travis/publishToArtifactory.sh
+++ travis/publishToArtifactory.sh
@@ -3,4 +3,4 @@
 mkdir assetsToPublish
 NPM_PACKAGE=$(npm pack)
 tar -xf $NPM_PACKAGE -C ./assetsToPublish --strip-components 2
-node travis/publishFolderToArtifactory $ARTIFACTORY_API_KEY ./assetsToPublish /artifactory/generic-local/${TRAVIS_TAG#"v"}
+node travis/publishFolderToArtifactory $ARTIFACTORY_API_KEY ./assetsToPublish /artifactory/web/${TRAVIS_TAG#"v"}
