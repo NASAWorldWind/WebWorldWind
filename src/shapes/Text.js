109b8d3be9503b7befbd5349d1733ed89835f11c
David Collins <paul@paulcollinssoftware.com>
2017-11-29 14:32:16 -0800

    Refactored Text's drawing code to improve maintainabililty - Consolidated unnecessarily repeated WebGL calls - Grouped related WebGL calls - Split independent drawing steps into separate functions

--- src/shapes/Text.js
+++ src/shapes/Text.js
@@ -184,9 +184,6 @@ define([
             this.imageTransform = Matrix.fromIdentity();
 
             // Internal use only. Intentionally not documented.
-            this.texCoordMatrix = Matrix.fromIdentity();
-
-            // Internal use only. Intentionally not documented.
             this.imageBounds = null;
 
             // Internal use only. Intentionally not documented.
@@ -431,9 +428,6 @@ define([
 
             // Suppress depth-buffer writes.
             gl.depthMask(false);
-
-            // The currentTexture field is used to avoid re-specifying textures unnecessarily. Clear it to start.
-            Text.currentTexture = null;
         };
 
         // Internal. Intentionally not documented.
@@ -441,25 +435,22 @@ define([
             var gl = dc.currentGlContext,
                 program = dc.currentProgram;
 
-            // Clear the vertex attribute state.
+            // Restore the default GL vertex attribute state.
             gl.disableVertexAttribArray(program.vertexPointLocation);
             gl.disableVertexAttribArray(program.vertexTexCoordLocation);
 
-            // Clear GL bindings.
+            // Restore the default GL buffer and texture bindings.
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
             gl.bindTexture(gl.TEXTURE_2D, null);
 
+            // Restore the default GL depth mask state.
             gl.depthMask(true);
-
-            // Avoid keeping a dangling reference to the current texture.
-            Text.currentTexture = null;
         };
 
         // Internal. Intentionally not documented.
         Text.prototype.doDrawOrderedText = function (dc) {
             var gl = dc.currentGlContext,
-                program = dc.currentProgram,
-                textureBound;
+                program = dc.currentProgram;
 
             // Compute the text's current visibility, potentially requesting additional frames.
             if (!dc.pickingMode && this.currentVisibility !== this.targetVisibility) {
@@ -470,6 +461,11 @@ define([
                     this.currentVisibility = Math.max(0, this.currentVisibility - visibilityDelta);
                 }
                 dc.redrawRequested = true;
+            }
+
+            // Turn off depth testing for the text unless it's been requested.
+            if (!this.activeAttributes.depthTest) {
+                gl.disable(gl.DEPTH_TEST);
             }
 
             // Use the text color and opacity. When picking, use the pick color, 100% opacity and no texture.
@@ -485,84 +481,78 @@ define([
 
             // When the text is visible, draw the text label.
             if (this.currentVisibility > 0) {
-                // Compute and specify the MVP matrix.
-                Text.matrix.copy(dc.screenProjection);
-                Text.matrix.multiplyMatrix(this.imageTransform);
-                program.loadModelviewProjection(gl, Text.matrix);
-
-                if (!dc.pickingMode) {
-                    this.texCoordMatrix.setToIdentity();
-                    if (this.activeTexture) {
-                        this.texCoordMatrix.multiplyByTextureTransform(this.activeTexture);
-                    }
-                    program.loadTextureMatrix(gl, this.texCoordMatrix);
-
-                    // Avoid unnecessary texture state changes
-                    if (this.activeTexture && this.activeTexture != Text.currentTexture) {
-                        textureBound = this.activeTexture.bind(dc); // returns false if texture is null or cannot be bound
-                        program.loadTextureEnabled(gl, textureBound);
-                        Text.currentTexture = this.activeTexture;
-                    }
-                }
-
-                // Turn off depth testing for the label unless it's been requested.
-                if (!this.activeAttributes.depthTest) {
-                    gl.disable(gl.DEPTH_TEST, false);
-                }
-                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-                if (!this.activeAttributes.depthTest) {
-                    // Turn depth testing back on.
-                    gl.disable(gl.DEPTH_TEST, true);
-                }
+                this.drawLabel(dc);
             }
 
             // When the text is not visible, draw a marker to indicate that something is there.
             if (this.currentVisibility < 1 && this.markerImageSource) {
-                var markerTexture = dc.gpuResourceCache.resourceForKey(this.markerImageSource);
-                if (!markerTexture) {
-                    dc.gpuResourceCache.retrieveTexture(dc.currentGlContext, this.markerImageSource);
-                    return;
-                }
-
-                var s = this.markerImageScale;
-                var markerTransform = Matrix.fromIdentity();
-                markerTransform.setTranslation(
-                    this.screenPoint[0] - s * markerTexture.imageWidth / 2,
-                    this.screenPoint[1] - s * markerTexture.imageWidth / 2,
-                    this.screenPoint[2]);
-
-                markerTransform.setScale(markerTexture.imageWidth * s, markerTexture.imageHeight * s, 1);
-
-                Text.matrix.copy(dc.screenProjection);
-                Text.matrix.multiplyMatrix(markerTransform);
-                program.loadModelviewProjection(gl, Text.matrix);
-
-                // Use the marker opacity and texture when not picking.
-                if (!dc.pickingMode) {
-                    program.loadOpacity(gl, this.layer.opacity * ( 1 - this.currentVisibility));
-
-                    var tcMatrix = Matrix.fromIdentity();
-                    tcMatrix.multiplyByTextureTransform(markerTexture);
-                    program.loadTextureMatrix(gl, tcMatrix);
-
-                    // Avoid unnecessary texture state changes
-                    if (markerTexture != Text.currentTexture) {
-                        textureBound = markerTexture.bind(dc); // returns false if texture is null or cannot be bound
-                        program.loadTextureEnabled(gl, textureBound);
-                        Text.currentTexture = markerTexture;
-                    }
-                }
-
-                // Turn off depth testing unless it's been requested.
-                if (!this.activeAttributes.depthTest) {
-                    gl.disable(gl.DEPTH_TEST, false);
-                }
-                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-                if (!this.activeAttributes.depthTest) {
-                    // Turn depth testing back on.
-                    gl.enable(gl.DEPTH_TEST, true);
-                }
+                this.drawMarker(dc);
             }
+
+            // Restore the default GL depth test state.
+            if (!this.activeAttributes.depthTest) {
+                gl.enable(gl.DEPTH_TEST);
+            }
+        };
+
+        // Internal. Intentionally not documented.
+        Text.prototype.drawLabel = function (dc) {
+            var gl = dc.currentGlContext,
+                program = dc.currentProgram,
+                textureBound;
+
+            // Use the label texture when not picking.
+            if (!dc.pickingMode && this.activeTexture) {
+                Text.matrix.setToIdentity();
+                Text.matrix.multiplyByTextureTransform(this.activeTexture);
+                textureBound = this.activeTexture.bind(dc); // returns false if texture is null or cannot be bound
+                program.loadTextureEnabled(gl, textureBound);
+                program.loadTextureMatrix(gl, Text.matrix);
+            }
+
+            // Compute and specify the text label's modelview-projection matrix.
+            Text.matrix.copy(dc.screenProjection);
+            Text.matrix.multiplyMatrix(this.imageTransform);
+            program.loadModelviewProjection(gl, Text.matrix);
+
+            // Draw the text as a two-triangle square.
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        };
+
+        // Internal. Intentionally not documented.
+        Text.prototype.drawMarker = function (dc) {
+            var gl = dc.currentGlContext,
+                program = dc.currentProgram,
+                textureBound;
+
+            var markerTexture = dc.gpuResourceCache.resourceForKey(this.markerImageSource);
+            if (!markerTexture) {
+                dc.gpuResourceCache.retrieveTexture(dc.currentGlContext, this.markerImageSource);
+                return;
+            }
+
+            // Use the marker opacity and texture when not picking.
+            if (!dc.pickingMode) {
+                Text.matrix.setToIdentity();
+                Text.matrix.multiplyByTextureTransform(markerTexture);
+                textureBound = markerTexture.bind(dc); // returns false if texture is null or cannot be bound
+                program.loadTextureEnabled(gl, textureBound);
+                program.loadTextureMatrix(gl, Text.matrix);
+                program.loadOpacity(gl, this.layer.opacity * (1 - this.currentVisibility));
+            }
+
+            // Compute and specify the marker's modelview-projection matrix.
+            var s = this.markerImageScale;
+            Text.matrix.copy(dc.screenProjection);
+            Text.matrix.multiplyByTranslation(
+                this.screenPoint[0] - s * markerTexture.imageWidth / 2,
+                this.screenPoint[1] - s * markerTexture.imageWidth / 2,
+                this.screenPoint[2]);
+            Text.matrix.multiplyByScale(markerTexture.imageWidth * s, markerTexture.imageHeight * s, 1);
+            program.loadModelviewProjection(gl, Text.matrix);
+
+            // Draw the marker as a two-triangle square.
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
         };
 
         return Text;
