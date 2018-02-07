ab696963e3f74f58f2d8db9e92fc4d1de870b4b5
Miguel Del Castillo <migueldelcastillo@live.com>
2018-02-07 08:56:43 -0600

    Changed OSM tile server back to Mapquest  - OSM's server was used for testing purposes only

--- src/layer/OpenStreetMapImageLayer.js
+++ src/layer/OpenStreetMapImageLayer.js
@@ -58,7 +58,7 @@ define([
             this.urlBuilder = {
                 urlForTile: function (tile, imageFormat) {
                     //var url = "https://a.tile.openstreetmap.org/" +
-                    return "https://a.tile.openstreetmap.org/" +
+                    return "otile1.mqcdn.com/tiles/1.0.0/osm/" +
                         (tile.level.levelNumber + 1) + "/" + tile.column + "/" + tile.row + ".png";
                 }
             };
