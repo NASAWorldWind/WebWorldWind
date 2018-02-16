2af7345becfb6dac2487545dc3a34fe2a585c8ce
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2018-02-16 16:48:23 -0600

    Change OSM data source

--- src/layer/OpenStreetMapImageLayer.js
+++ src/layer/OpenStreetMapImageLayer.js
@@ -17,79 +17,80 @@
  * @exports OpenStreetMapImageLayer
  */
 define([
-        '../geom/Angle',
         '../util/Color',
-        '../geom/Location',
-        '../geom/Sector',
-        '../layer/MercatorTiledImageLayer'
+        '../util/Logger',
+        '../ogc/wmts/WmtsCapabilities',
+        '../layer/WmtsLayer'
     ],
-    function (Angle,
-              Color,
-              Location,
-              Sector,
-              MercatorTiledImageLayer) {
+    function (Color,
+              Logger,
+              WmtsCapabilities,
+              WmtsLayer) {
         "use strict";
 
         /**
          * Constructs an Open Street Map layer.
          * @alias OpenStreetMapImageLayer
          * @constructor
-         * @augments MercatorTiledImageLayer
+         * @augments WmtsLayer
          * @classdesc Provides a layer that shows Open Street Map imagery.
          *
          * @param {String} displayName This layer's display name. "Open Street Map" if this parameter is
          * null or undefined.
          */
         var OpenStreetMapImageLayer = function (displayName) {
-            this.imageSize = 256;
             displayName = displayName || "Open Street Map";
 
-            MercatorTiledImageLayer.call(this,
-                new Sector(-85.05, 85.05, -180, 180), new Location(85.05, 180), 19, "image/png", displayName,
-                this.imageSize, this.imageSize);
+            var xhr = new XMLHttpRequest();
+            var url = "https://tiles.maps.eox.at/wmts/1.0.0/WMTSCapabilities.xml";
+
+            var capabilitiesDocument = function (ctx) {
+                var that = ctx;
+                return {
+                    retrieveAndParse: function () {
+                        if (xhr.readyState === 4) {
+                            if (xhr.status === 200) {
+                                var wmtsCapabilities = new WmtsCapabilities(xhr.responseXML);
+                                var wmtsLayerCapabilities = wmtsCapabilities.getLayer("osm");
+                                var wmtsConfig = WmtsLayer.formLayerConfiguration(wmtsLayerCapabilities);
+                                wmtsConfig.title = "Open Street Map";
+                                WmtsLayer.call(that, wmtsConfig);
+                            } else {
+                                Logger.log(Logger.LEVEL_WARNING,
+                                    "OSM retrieval failed (" + xhr.statusText + "): " + url);
+                            }
+                        }
+                    }
+                }
+            };
+
+            xhr.open("GET", url, true);
+            xhr.onreadystatechange = capabilitiesDocument(this).retrieveAndParse;
+
+            xhr.onerror = function () {
+                Logger.log(Logger.LEVEL_WARNING, "OSM retrieval failed: " + url);
+            };
+
+            xhr.ontimeout = function () {
+                Logger.log(Logger.LEVEL_WARNING, "OSM retrieval timed out: " + url);
+            };
+
+            xhr.send(null);
 
             this.displayName = displayName;
             this.pickEnabled = false;
-
-            // Create a canvas we can use when unprojecting retrieved images.
-            this.destCanvas = document.createElement("canvas");
-            this.destContext = this.destCanvas.getContext("2d");
-
-            this.urlBuilder = {
-                urlForTile: function (tile, imageFormat) {
-                    //var url = "https://a.tile.openstreetmap.org/" +
-                    return "https://otile1.mqcdn.com/tiles/1.0.0/osm/" +
-                        (tile.level.levelNumber + 1) + "/" + tile.column + "/" + tile.row + ".png";
-                }
-            };
         };
 
-        OpenStreetMapImageLayer.prototype = Object.create(MercatorTiledImageLayer.prototype);
+        OpenStreetMapImageLayer.prototype = Object.create(WmtsLayer.prototype);
 
         OpenStreetMapImageLayer.prototype.doRender = function (dc) {
-            MercatorTiledImageLayer.prototype.doRender.call(this, dc);
+            WmtsLayer.prototype.doRender.call(this, dc);
             if (this.inCurrentFrame) {
-                dc.screenCreditController.addStringCredit("\u00A9OpenStreetMap", Color.DARK_GRAY);
-                dc.screenCreditController.addStringCredit("Tiles Courtesy of MapQuest", Color.DARK_GRAY);
+                dc.screenCreditController.addStringCredit("(Contains modified Copernicus Sentinel data 2016 & 2017)", Color.DARK_GRAY);
+                dc.screenCreditController.addStringCredit("Sentinel-2 cloudless - https://s2maps.eu by EOX IT Services GmbH ", Color.DARK_GRAY);
             }
-        };
-
-        // Overridden from TiledImageLayer.
-        OpenStreetMapImageLayer.prototype.createTopLevelTiles = function (dc) {
-            this.topLevelTiles = [];
-
-            this.topLevelTiles.push(this.createTile(null, this.levels.firstLevel(), 0, 0));
-            this.topLevelTiles.push(this.createTile(null, this.levels.firstLevel(), 0, 1));
-            this.topLevelTiles.push(this.createTile(null, this.levels.firstLevel(), 1, 0));
-            this.topLevelTiles.push(this.createTile(null, this.levels.firstLevel(), 1, 1));
-        };
-
-        // Determines the Bing map size for a specified level number.
-        OpenStreetMapImageLayer.prototype.mapSizeForLevel = function (levelNumber) {
-            return 256 << (levelNumber + 1);
         };
 
         return OpenStreetMapImageLayer;
     }
-)
-;
+);
