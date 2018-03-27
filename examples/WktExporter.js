247549c3506f7d77731df18809385a19578be86d
AkeluX <374540+AkeluX@users.noreply.github.com>
2018-03-27 17:21:29 +0200

    Clean up the headers and the example

--- examples/WktExporter.js
+++ examples/WktExporter.js
@@ -1,25 +1,24 @@
 /*
-* Copyright 2015-2017 WorldWind Contributors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-/**
- * Basic example for Wkt Exporter
+ * Copyright 2015-2018 WorldWind Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-requirejs(['../src/WorldWind',
+/**
+ * Illustrates how to export surface shapes in Well-Know Text.
+ */
+requirejs(['./WorldWindShim',
         './LayerManager'],
-    function (ww,
+    function (WorldWind,
               LayerManager) {
         "use strict";
 
@@ -43,19 +42,15 @@ requirejs(['../src/WorldWind',
             wwd.addLayer(layers[l].layer);
         }
 
-        // Create a layer to hold the surface shapes.
+        // Create a layer and some surface shapes
         var shapesLayer = new WorldWind.RenderableLayer("Surface Shapes");
         wwd.addLayer(shapesLayer);
 
-        // Create a simple surface polygon, a triangle.
         var boundary = [];
         boundary.push(new WorldWind.Location(40, -100));
         boundary.push(new WorldWind.Location(45, -110));
         boundary.push(new WorldWind.Location(40, -120));
 
-        // Create and set attributes for it. The shapes below except the surface polyline use this same attributes
-        // object. Real apps typically create new attributes objects for each shape unless they know the attributes
-        // can be shared among shapes.
         var attributes = new WorldWind.ShapeAttributes(null);
         attributes.outlineColor = WorldWind.Color.BLUE;
         attributes.interiorColor = new WorldWind.Color(1, 1, 1, 1.0);
@@ -108,7 +103,10 @@ requirejs(['../src/WorldWind',
         shape = new WorldWind.SurfaceSector(new WorldWind.Sector(33, 37, -88, -83), attributes);
         shapesLayer.addRenderable(shape);
 
+        // Export the surface shapes on click
         function onExportWkt() {
+
+            // This is the actual export action.
             var exportedWkt = WorldWind.WktExporter.exportRenderables(shapesLayer.renderables);
 
             document.getElementById("wktTxtArea").value = exportedWkt;
