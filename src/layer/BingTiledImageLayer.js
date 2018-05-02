2402f66b98c57dfd9f0c90552ba9bf88d8f084c5
Miguel Del Castillo <migueldelcastillo@live.com>
2018-05-01 23:26:12 -0500

    Replaced image credit with ScreenImage of Bing's logo  - Provided responsive placement in the top left of the screen

--- src/layer/BingTiledImageLayer.js
+++ src/layer/BingTiledImageLayer.js
@@ -18,14 +18,22 @@
  */
 define([
         '../geom/Angle',
+        '../util/Color',
         '../geom/Location',
+        '../util/Offset',
+        '../shapes/ScreenImage',
         '../geom/Sector',
-        '../layer/MercatorTiledImageLayer'
+        '../layer/MercatorTiledImageLayer',
+        '../layer/ViewControlsLayer'
     ],
     function (Angle,
+              Color,
               Location,
+              Offset,
+              ScreenImage,
               Sector,
-              MercatorTiledImageLayer) {
+              MercatorTiledImageLayer,
+              ViewControlsLayer) {
         "use strict";
 
         /**
@@ -35,11 +43,17 @@ define([
          * @augments MercatorTiledImageLayer
          * @classdesc Provides an abstract base layer for Bing imagery. This class is not intended to be constructed
          * independently but as a base layer for subclasses.
+         * @param (logoOffset) Offset Optional offset indicating the Bing logo attribution placement on the screen.
+         * @param (logoPosition) String Optional positioning of the Bing logo in the left canvas side, either top or
+         * bottom of the canvas. Defaults to top.
          * See {@link BingAerialLayer}, {@link BingAerialWithLabelsLayer} and {@link BingRoadsLayer}.
          *
          * @param {String} displayName This layer's display name.
          */
-        var BingTiledImageLayer = function (displayName) {
+        var BingTiledImageLayer = function (displayName, logoPosition) {
+
+            var logotypePosition = logoPosition ? logoPosition : "top";
+
             this.imageSize = 256;
 
             MercatorTiledImageLayer.call(this,
@@ -53,15 +67,47 @@ define([
 
             this.detectBlankImages = true;
 
-            this.creditImage = WorldWind.configuration.baseUrl + "images/powered-by-bing.png";
+            this.logoImage = WorldWind.configuration.baseUrl + "images/powered-by-bing.png";
+
+            this.logo = this.createLogotype();
+
+            this.logoPosition = logotypePosition;
         };
 
         BingTiledImageLayer.prototype = Object.create(MercatorTiledImageLayer.prototype);
 
         BingTiledImageLayer.prototype.doRender = function (dc) {
+            var canvasWidth = dc.currentGlContext.canvas.clientWidth;
+
+            // Draw Bing logo in selected position.
+            if (this.logoPosition = "top") {
+                // Top left was selected. Draw in top left corner when CoordinatesDisplayLayer is being drawn at the
+                // bottom of the screen
+
+                this.logo.screenOffset.xUnits = WorldWind.OFFSET_FRACTION;
+                this.logo.screenOffset.x = 0;
+                this.logo.screenOffset.yUnits = WorldWind.OFFSET_INSET_PIXELS;
+                this.logo.imageOffset.y = 1;
+                this.logo.imageOffset.x = 0;
+
+                if (canvasWidth >= 650) { // Large canvas, draw logo in upper left corner
+                    this.logo.screenOffset.y = 0;
+                } else { // Medium/Small canvas, offset vertically to avoid cluttering with CoordinatesDisplayLayer
+                    this.logo.screenOffset.y = 21;
+                }
+
+            } else if (this.logoPosition = "bottom") {
+                // Bottom left was selected. Draw in lower left corner when the view controls are not visible, or
+                // right above them when they are.
+                // dc.layers.indexOf("ViewControlsLayer")
+
+            }
+
+
+
             MercatorTiledImageLayer.prototype.doRender.call(this, dc);
             if (this.inCurrentFrame) {
-                dc.screenCreditController.addImageCredit(this.creditImage);
+                this.logo.render(dc);
             }
         };
 
@@ -78,6 +124,16 @@ define([
         // Determines the Bing map size for a specified level number.
         BingTiledImageLayer.prototype.mapSizeForLevel = function (levelNumber) {
             return 256 << (levelNumber + 1);
+        };
+
+        BingTiledImageLayer.prototype.createLogotype = function () {
+            var offset = new Offset(WorldWind.OFFSET_FRACTION, 0, WorldWind.OFFSET_FRACTION, 0);
+            var logotype = new ScreenImage(offset, this.logoImage);
+
+            logotype.imageColor = new Color(1, 1, 1, 0.5);
+
+
+            return logotype;
         };
 
         return BingTiledImageLayer;
