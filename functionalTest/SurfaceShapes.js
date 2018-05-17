80b9d568163b1f731630dda90028c5ef9f9de8c9
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2018-05-17 12:55:17 -0500

    Update layout

--- functionalTest/SurfaceShapes.js
+++ functionalTest/SurfaceShapes.js
@@ -8,14 +8,27 @@ requirejs([
         var statusOutput = document.getElementById("output");
         var globeCanvas = document.getElementById("globe");
 
-        statusDialog.innerText = "Initializing globe...";
+        var updateStatus = function (statusMessage) {
+            var children = statusDialog.childNodes;
+            for (var c = 0; c < children.length; c++) {
+                statusDialog.removeChild(children[c]);
+            }
+
+            statusDialog.appendChild(document.createTextNode(statusMessage));
+        };
+
+        updateStatus("Initializing globe...");
 
         var wwd = new WorldWind.WorldWindow(globeCanvas);
-        wwd.globe.elevationModel.removeAllCoverages(); // Don't want delays associated with changing terrain
+        wwd.globe.elevationModel.removeAllCoverages(); // Don't want delays associated with parsing and changing terrain
         var bmnglayer = new WorldWind.BMNGOneImageLayer();
         bmnglayer.minActiveAltitude = 0;
         wwd.addLayer(bmnglayer); // Don't want any imaging processing delays
 
-        statusDialog.innerText = "Globe Loaded";
+        updateStatus("Globe Loaded, ready for testing");
+
+        var runNavigation = function () {
+
+        };
 
     });
