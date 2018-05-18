f2760289c8cedcaf32c98a7c7041e8c0c93df98e
Miguel Del Castillo <migueldelcastillo@live.com>
2018-05-18 12:59:30 -0500

    Repositioned 2D overlays  - ViewControlsLayer now has a 10px margin (instead of 0px) over lower    right corner.  - CoordinatesDisplayLayer now is 10px above canvas bottom (instead of    5px) when in 'large' canvas mode.  - Bing logo now has margin of 7px (instead of 5px) in lower right corner.

--- src/layer/ViewControlsLayer.js
+++ src/layer/ViewControlsLayer.js
@@ -108,7 +108,7 @@ define([
              * @type {Offset}
              * @default The lower left corner of the window.
              */
-            this.placement = new Offset(WorldWind.OFFSET_FRACTION, 0, WorldWind.OFFSET_FRACTION, 0);
+            this.placement = new Offset(WorldWind.OFFSET_PIXELS, 10, WorldWind.OFFSET_PIXELS, 10);
 
             /**
              * An {@link Offset} indicating the alignment of the control collection relative to the
