5ca89e7281a02db06fbf8730a9b8cf13d22adc85
Zach Glueckert <zachary.r.glueckert@nasa.gov>
2018-05-21 15:07:58 -0500

    Add dynamic shapes

--- functionalTest/SurfaceShapes.js
+++ functionalTest/SurfaceShapes.js
@@ -7,17 +7,39 @@ requirejs([
 
         var navigateButton = document.getElementById("navigate-button");
         var staticShapesButton = document.getElementById("static-button");
+        var dynamicShapesButton = document.getElementById("dynamic-button");
 
         var wwd = Util.initializeLowResourceWorldWindow("globe");
+        var testLayer = new WorldWind.RenderableLayer("Test Layer");
+        wwd.addLayer(testLayer);
         var util = new Util(wwd);
 
         util.setStatusMessage("Globe loaded, ready for testing...");
 
-        // moving state
-        var moving = false;
-        var navigationComplete = false;
-
         // moves
+        var moveZero = {
+            latitude: {
+                goal: wwd.navigator.lookAtLocation.latitude,
+                step: Number.MAX_VALUE
+            },
+            longitude: {
+                goal: wwd.navigator.lookAtLocation.longitude,
+                step: Number.MAX_VALUE
+            },
+            tilt: {
+                goal: 0,
+                step: Number.MAX_VALUE
+            },
+            heading: {
+                goal: 0,
+                step: Number.MAX_VALUE
+            },
+            range: {
+                goal: wwd.navigator.range,
+                step: Number.MAX_VALUE
+            }
+        };
+
         var moveOne = {
             range: {
                 goal: 8e5,
@@ -34,8 +56,8 @@ requirejs([
 
         var moveTwo = {
             heading: {
-                goal: 135,
-                step: 1
+                goal: 90,
+                step: 0.75
             },
             onComplete: function () {
                 util.setStatusMessage("Second move complete");
@@ -49,57 +71,187 @@ requirejs([
             },
             longitude: {
                 goal: -70,
-                step: 0.25
+                step: 0.5
             },
             onComplete: function () {
-                moving = false;
-                navigationComplete = true;
+                util.stopMetricCapture();
                 util.setStatusMessage("Move Complete");
+
+                // output metrics
+                var stats = util.frameStats.map(function (frameStat) {
+                    return frameStat.frameTime;
+                });
+                stats.shift();
+                stats.pop();
+                util.setOutputMessage(Util.generateResultsSummary(stats, "Frame Time"));
+                stats = util.frameStats.map(function (frameStat) {
+                    return frameStat.layerRenderingTime;
+                });
+                stats.shift();
+                stats.pop();
+                util.appendOutputMessage(Util.generateResultsSummary(stats, "Layer Rendering Time"));
             }
         };
 
-        var onClickMove = function () {
-            if (!moving) {
-                moving = true;
-                util.move([moveOne, moveTwo, moveThree]);
+
+
+        var generateShapeAttributes = function () {
+            var sa = new WorldWind.ShapeAttributes();
+            var r = Math.random();
+
+            sa.drawInterior = false;
+            sa.drawOutline = false;
+
+            if (r < 0.3333) {
+                sa.drawInterior = true;
+                sa.interiorColor = new WorldWind.Color(Math.random(), Math.random(), Math.random(), Math.random());
+            } else if (r < 0.6666) {
+                sa.drawOutline = true;
+                sa.outlineColor = new WorldWind.Color(Math.random(), Math.random(), Math.random(), Math.random());
+            } else {
+                sa.drawInterior = true;
+                sa.interiorColor = new WorldWind.Color(Math.random(), Math.random(), Math.random(), Math.random());
+                sa.drawOutline = true;
+                sa.outlineColor = new WorldWind.Color(Math.random(), Math.random(), Math.random(), Math.random());
             }
+
+            return sa;
         };
 
-        var onGenerateShapesClick = function () {
-            var layer = new WorldWind.RenderableLayer("Test Layer");
-            wwd.addLayer(layer);
-            for (var i = 0; i < 10000; i++) {
-                var shapeLocation = new WorldWind.Location(Math.random() * 160 - 80, Math.random() * 340 - 170);
-                var minorAxis = Math.random() * 100000 + 10000;
-                var majorAxis = Math.random() * 500000 + 10000;
-                var heading = Math.random() * 360;
-                layer.addRenderable(new WorldWind.SurfaceEllipse(shapeLocation, majorAxis, minorAxis, heading));
+        var generateLocation = function () {
+            var lat = 180 * Math.random() - 90;
+            var lon = 360 * Math.random() - 180;
+
+            return new WorldWind.Location(lat, lon);
+        };
+
+        var generateShapes = function (count) {
+            var sa, shape, sector, radius, topLocation, lat, lon, locations, minorAxis, majorAxis, heading, i, j;
+
+            // Surface Circles
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                radius = Math.random() * 1000000;
+                shape = new WorldWind.SurfaceCircle(generateLocation(), radius, sa);
+                testLayer.addRenderable(shape);
             }
+
+            // Surface Ellipse
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                majorAxis = Math.random() * 1000000;
+                minorAxis = Math.random() * 500000;
+                heading = Math.random() * 360;
+                shape = new WorldWind.SurfaceEllipse(generateLocation(), majorAxis, minorAxis, heading, sa);
+                testLayer.addRenderable(shape);
+            }
+
+            // Surface Polygon
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                topLocation = generateLocation();
+                locations = [];
+                locations.push(topLocation);
+                for (j = 0; j < 2; j++) {
+                    lat = Math.min(90, Math.max(-90, topLocation.latitude - Math.random() * 8));
+                    lon = Math.min(180, Math.max(-180, topLocation.longitude - Math.random() * 8));
+                    locations.push(new WorldWind.Location(lat, lon));
+                }
+                shape = new WorldWind.SurfacePolygon(locations, sa);
+                testLayer.addRenderable(shape);
+            }
+
+            // Surface Polyline
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                topLocation = generateLocation();
+                locations = [];
+                locations.push(topLocation);
+                for (j = 0; j < 2; j++) {
+                    lat = Math.min(90, Math.max(-90, topLocation.latitude - Math.random() * 8));
+                    lon = Math.min(180, Math.max(-180, topLocation.longitude - Math.random() * 8));
+                    locations.push(new WorldWind.Location(lat, lon));
+                }
+                shape = new WorldWind.SurfacePolyline(locations, sa);
+                testLayer.addRenderable(shape);
+            }
+
+            // Surface Rectangle
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                majorAxis = 1000000 * Math.random();
+                minorAxis = 500000 * Math.random();
+                heading = 360 * Math.random();
+                shape = new WorldWind.SurfaceRectangle(generateLocation(), majorAxis, minorAxis, heading, sa);
+                testLayer.addRenderable(shape);
+            }
+
+            // Surface Sector
+            for (i = 0; i < count; i++) {
+                sa = generateShapeAttributes();
+                topLocation = generateLocation();
+                majorAxis = 8 * Math.random();
+                minorAxis = 4 * Math.random();
+                sector = new WorldWind.Sector(topLocation.latitude, topLocation.latitude + majorAxis,
+                    topLocation.longitude, topLocation.longitude + minorAxis);
+                shape = new WorldWind.SurfaceSector(sector, sa);
+            }
+
             wwd.redraw();
         };
 
-        // Temporary metric capture framework
-        var stats = [], min = Number.MAX_VALUE, max = -Number.MAX_VALUE;
-        var metricCapture = function (worldwindow, stage) {
-            if (stage === WorldWind.AFTER_REDRAW) {
-                if (moving && !navigationComplete) {
-                    var frametime = worldwindow.frameStatistics.frameTime;
-                    stats.push(frametime);
-                    min = Math.min(min, frametime);
-                    max = Math.max(max, frametime);
-                }
-
-                if (navigationComplete) {
-                    navigationComplete = false;
-                    // clean up the data
-                    stats.shift();
-                    stats.pop();
-                    util.setOutputMessage(Util.generateResultsSummary(stats, "Frame Times"));
-                }
+        var onMoveClick = function () {
+            if (!util.isMoving()) {
+                util.startMetricCapture();
+                util.move([moveZero, moveOne, moveTwo, moveThree]);
             }
         };
-        wwd.redrawCallbacks.push(metricCapture);
 
-        navigateButton.addEventListener("click", onClickMove);
-        staticShapesButton.addEventListener("click", onGenerateShapesClick);
+        var onStaticShapesClick = function () {
+            generateShapes(1000);
+        };
+
+        var onDynamicShapesClick = function () {
+            generateShapes(100);
+
+            var onRedrawMoveShapes = function (worldwindow, stage) {
+                if (stage === WorldWind.AFTER_REDRAW) {
+                    var idx = worldwindow.layers.indexOf(testLayer);
+
+                    if (idx >= 0) {
+                        var count = worldwindow.layers[idx].renderables.length;
+
+                        for (var i = 0; i < count; i++) {
+                            var shape = worldwindow.layers[idx].renderables[i];
+
+                            if (shape instanceof WorldWind.SurfaceCircle || shape instanceof WorldWind.SurfaceEllipse
+                                || shape instanceof WorldWind.SurfaceRectangle) {
+                                var center = shape.center;
+                                center.latitude += Math.random() * 0.5 - 0.25;
+                                center.longitude += Math.random() * 0.5 - 0.25;
+                                shape.center = center;
+                            } else if (shape instanceof WorldWind.SurfacePolygon
+                                || shape instanceof WorldWind.SurfacePolyline) {
+                                var boundaries = shape.boundaries;
+                                boundaries[0].latitude += Math.random() * 0.5 - 0.25;
+                                boundaries[0].longitude += Math.random() * 0.5 - 0.25;
+                                shape.boundaries = boundaries;
+                            } else if (shape instanceof WorldWind.SurfaceSector) {
+                                var sector = shape.sector;
+                                sector.minLongitude += Math.random() * 0.5 - 0.25;
+                                sector.minLatitude += Math.random() * 0.5 - 0.25;
+                                shape.sector = sector;
+                            }
+
+                        }
+                    }
+                }
+            };
+
+            wwd.redrawCallbacks.push(onRedrawMoveShapes);
+        };
+
+        navigateButton.addEventListener("click", onMoveClick);
+        staticShapesButton.addEventListener("click", onStaticShapesClick);
+        dynamicShapesButton.addEventListener("click", onDynamicShapesClick);
     });
