71b431bc26f0c394c53e7448b498af8cb5e36638
Miguel Del Castillo <migueldelcastillo@live.com>
2018-05-23 14:50:08 -0500

    Commented boilerplate code for examples (from Paths to WMTS). (#709)
    * Commented WorldWindow boilerplate (logger and layer creation and addition).
    
    * Corrected comment in StarField

--- examples/RefreshImagery.js
+++ examples/RefreshImagery.js
@@ -23,14 +23,19 @@ requirejs(['./WorldWindShim',
               LayerManager) {
         "use strict";
 
+        // Tell WorldWind to log only warnings.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
+        // Create the WorldWindow.
         var wwd = new WorldWind.WorldWindow("canvasOne");
 
         var layerToRefresh = new WorldWind.BMNGLayer();
 
+        // Create and add layers to the WorldWindow, including our previously created layer to be refreshed.
         var layers = [
+            // Imagery layer.
             {layer: layerToRefresh, enabled: true},
+            // WorldWindow UI layers.
             {layer: new WorldWind.CompassLayer(), enabled: true},
             {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
             {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
--- examples/WMTS.js
+++ examples/WMTS.js
@@ -22,17 +22,21 @@ requirejs([
               LayerManager) {
         "use strict";
 
+        // Tell WorldWind to log only warnings.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
+        // Create the WorldWindow.
         var wwd = new WorldWind.WorldWindow("canvasOne");
 
-        // Standard WorldWind layers
+        // Create and add layers to the WorldWindow.
         var layers = [
+            // Imagery layers.
             {layer: new WorldWind.BMNGLayer(), enabled: true},
             {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
             {layer: new WorldWind.BingAerialLayer(null), enabled: false},
             {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: false},
             {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
+            // WorldWindow UI layers.
             {layer: new WorldWind.CompassLayer(), enabled: true},
             {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
             {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
