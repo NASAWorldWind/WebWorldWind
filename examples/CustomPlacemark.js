203b43ce1e8744cc915c75bc907a07a61e997ce9
eirizarry <endiajanae@mail.fresnostate.edu>
2018-05-25 12:14:13 -0700

    Enhanced comments for Custom Placemark example

--- examples/CustomPlacemark.js
+++ examples/CustomPlacemark.js
@@ -45,21 +45,7 @@ requirejs(['./WorldWindShim',
             wwd.addLayer(layers[l].layer);
         }
 
-        var placemark,
-            placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
-            highlightAttributes,
-            placemarkLayer = new WorldWind.RenderableLayer("Placemarks"),
-            latitude = 47.684444,
-            longitude = -121.129722;
-
-        // Set up the common placemark attributes.
-        placemarkAttributes.imageScale = 1;
-        placemarkAttributes.imageOffset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 0.5);
-        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
-
-        // Create the custom image for the placemark.
+        // Create the custom image for the placemark with a 2D canvas.
         var canvas = document.createElement("canvas"),
             ctx2d = canvas.getContext("2d"),
             size = 64, c = size / 2 - 0.5, innerRadius = 5, outerRadius = 20;
@@ -76,22 +62,40 @@ requirejs(['./WorldWindShim',
         ctx2d.arc(c, c, outerRadius, 0, 2 * Math.PI, false);
         ctx2d.fill();
 
+        // Define placemark's geographic position.
+        var latitude = 47.684444;
+        var longitude = -121.129722;
+
         // Create the placemark.
-        placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), false, null);
+        var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), false, null);
         placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
 
-        // Create the placemark attributes for the placemark.
+        // Set up the common placemark attributes.
+        var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+        placemarkAttributes.imageScale = 1;
+
+        // Define a point for the placemark to pivot to the defined geographic location. In this case, the point is centered.
+        placemarkAttributes.imageOffset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 0.5);
+        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
         placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+
         // Wrap the canvas created above in an ImageSource object to specify it as the placemark image source.
         placemarkAttributes.imageSource = new WorldWind.ImageSource(canvas);
+
+        //Assign all previously defined attributes to the placemark.
         placemark.attributes = placemarkAttributes;
 
         // Create the highlight attributes for this placemark. Note that the normal attributes are specified as
         // the default highlight attributes so that all properties are identical except the image scale. You could
         // instead vary the color, image, or other property to control the highlight representation.
-        highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+        var highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
         highlightAttributes.imageScale = 1.2;
         placemark.highlightAttributes = highlightAttributes;
+
+        // Create the renderable layer for placemarks.
+        var placemarkLayer = new WorldWind.RenderableLayer("Placemarks");
 
         // Add the placemark to the layer.
         placemarkLayer.addRenderable(placemark);
@@ -99,9 +103,10 @@ requirejs(['./WorldWindShim',
         // Add the placemarks layer to the WorldWindow's layer list.
         wwd.addLayer(placemarkLayer);
 
+        // Now set up to handle highlighting.
+        var highlightController = new WorldWind.HighlightController(wwd);
+
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
 
-        // Now set up to handle highlighting.
-        var highlightController = new WorldWind.HighlightController(wwd);
     });
