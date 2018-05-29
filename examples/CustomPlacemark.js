3183b90208871a4b26fcb8386c58aa1e5d74fcae
Miguel Del Castillo <migueldelcastillo@live.com>
2018-05-29 16:00:55 -0500

    Repositioned code to increase clarity and readability.  - Removed previous multiple PlacemarkAttributes object creation.

--- examples/CustomPlacemark.js
+++ examples/CustomPlacemark.js
@@ -62,40 +62,32 @@ requirejs(['./WorldWindShim',
         ctx2d.arc(c, c, outerRadius, 0, 2 * Math.PI, false);
         ctx2d.fill();
 
-        // Define placemark's geographic position.
-        var latitude = 47.684444;
-        var longitude = -121.129722;
-
-        // Create the placemark.
-        var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), false, null);
-        placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
-
-        // Set up the common placemark attributes.
+        // Set placemark attributes.
         var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-        placemarkAttributes.imageScale = 1;
-
-        // Define a point for the placemark to pivot to the defined geographic location. In this case, the point is centered.
-        placemarkAttributes.imageOffset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 0.5);
-        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
-        placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-
-        // Wrap the canvas created above in an ImageSource object to specify it as the placemark image source.
+        // Wrap the canvas created above in an ImageSource object to specify it as the placemarkAttributes image source.
         placemarkAttributes.imageSource = new WorldWind.ImageSource(canvas);
+        // Define the pivot point for the placemark at the center of its image source.
+        placemarkAttributes.imageOffset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0.5, WorldWind.OFFSET_FRACTION, 0.5);
+        placemarkAttributes.imageScale = 1;
+        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
 
-        //Assign all previously defined attributes to the placemark.
-        placemark.attributes = placemarkAttributes;
-
-        // Create the highlight attributes for this placemark. Note that the normal attributes are specified as
-        // the default highlight attributes so that all properties are identical except the image scale. You could
-        // instead vary the color, image, or other property to control the highlight representation.
+        // Set placemark highlight attributes.
+        // Note that the normal attributes are specified as the default highlight attributes so that all properties
+        // are identical except the image scale. You could instead vary the color, image, or other property
+        // to control the highlight representation.
         var highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
         highlightAttributes.imageScale = 1.2;
+
+        // Create the placemark with the attributes attributes defined above.
+        var latitude = 47.684444, longitude = -121.129722, altitude = 1e2;
+        var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, altitude), false, placemarkAttributes);
+        // Draw placemark at altitude defined above, relative to the terrain.
+        placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+        // Assign highlight attributes for the placemark.
         placemark.highlightAttributes = highlightAttributes;
 
         // Create the renderable layer for placemarks.
-        var placemarkLayer = new WorldWind.RenderableLayer("Placemarks");
+        var placemarkLayer = new WorldWind.RenderableLayer("Custom Placemark");
 
         // Add the placemark to the layer.
         placemarkLayer.addRenderable(placemark);
