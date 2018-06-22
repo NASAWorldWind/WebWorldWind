09c81f862e726e2a7edcb70373cc411dd8eb0def
Patrick Hogan <Patrick.Hogan@nasa.gov>
2018-06-21 19:03:52 -0700

    Update

--- test/util/XmlDocument.test.js
+++ test/util/XmlDocument.test.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2003-2006, 2009, 2017, United States Government, as represented by the Administrator of the
+ *  National Aeronautics and Space Administration. All rights reserved.
  * 
  * The NASAWorldWind/WebWorldWind platform is licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
