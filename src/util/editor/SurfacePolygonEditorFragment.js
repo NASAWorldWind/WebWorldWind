a15959d3a669e4e9b839f492bffdb34187948e91
Claudia Ifrim <ifrim.claudia@gmail.com>
2018-06-24 12:12:20 +0300

    fix shape editor constants

--- src/util/editor/SurfacePolygonEditorFragment.js
+++ src/util/editor/SurfacePolygonEditorFragment.js
@@ -62,7 +62,7 @@ define([
                 outer:
                     for (var i = 0; i < boundaries.length; i++) {
                         for (var j = 0; j < boundaries[i].length; j++) {
-                            if (controlPoint.userProperties.purpose === ShapeEditor.LOCATION) {
+                            if (controlPoint.userProperties.purpose === ShapeEditorConstants.LOCATION) {
                                 if (controlPoint.userProperties.id === k) {
                                     newPos = this.moveLocation(globe, controlPoint, terrainPosition, previousPosition);
                                     boundaries[i][j] = newPos;
@@ -71,7 +71,7 @@ define([
                                     break outer;
                                 }
                             }
-                            else if (controlPoint.userProperties.purpose === ShapeEditor.ROTATION) {
+                            else if (controlPoint.userProperties.purpose === ShapeEditorConstants.ROTATION) {
                                 this.rotateLocations(terrainPosition, boundaries);
                                 shape.boundaries = boundaries;
                                 break outer;
