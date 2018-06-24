723e2c6686482a53b60463c1c5d7913549688734
Claudia Ifrim <ifrim.claudia@gmail.com>
2018-06-24 11:44:50 +0300

    update copyright

--- src/util/editor/ShapeEditorConstants.js
+++ src/util/editor/ShapeEditorConstants.js
@@ -1,12 +1,11 @@
 /*
- * Copyright 2003-2006, 2009, 2017, United States Government, as represented by the Administrator of the
- * National Aeronautics and Space Administration. All rights reserved.
+ * Copyright 2015-2018 WorldWind Contributors
  *
- * The NASAWorldWind/WebWorldWind platform is licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
