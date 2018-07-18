ad0881bac543323346569ce20e69071dbc536db8
jbalhar <jakub@balhar.net>
2018-07-18 16:11:43 +0200

    Update the structure of the example.

--- examples/HeatMap.js
+++ examples/HeatMap.js
@@ -18,11 +18,30 @@ requirejs(['./WorldWindShim',
         './LayerManager'],
     function (WorldWind,
               LayerManager) {
+        "use strict";
+
+        // Tell WorldWind to log only warnings and errors.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
-        "use strict";
         // Create the WorldWindow.
         var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        // Create and add layers to the WorldWindow.
+        var layers = [
+            // Imagery layers.
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            // WorldWindow UI layers.
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
 
         // Generate 10000 random points to display on the HeatMap with varying intensity over the area of the whole world.
         var locations = [];
@@ -36,25 +55,9 @@ requirejs(['./WorldWindShim',
             );
         }
 
-        var heatMapLayer = new WorldWind.HeatMapLayer("HeatMap", locations);
-
-        var layers = [
-            {layer: new WorldWind.BMNGLayer(), enabled: true},
-            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
-            {layer: new WorldWind.CompassLayer(), enabled: true},
-            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true},
-            {layer: heatMapLayer, enabled: true}
-        ];
-
-        for (var l = 0; l < layers.length; l++) {
-            layers[l].layer.enabled = layers[l].enabled;
-            wwd.addLayer(layers[l].layer);
-        }
+        // Add new HeatMap Layer with the points as the data source.
+        wwd.addLayer(new WorldWind.HeatMapLayer("HeatMap", locations));
 
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
-
-        // Now set up to handle highlighting.
-        var highlightController = new WorldWind.HighlightController(wwd);
     });
