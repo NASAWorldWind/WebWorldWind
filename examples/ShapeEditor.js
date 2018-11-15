4e5312ffad8b6c8f336ea923a9564359667d3213
Claudia Ifrim <ifrim.claudia@gmail.com>
2018-11-15 16:36:24 +0200

    cleanup and add outlineWidth to highlighted attributes

--- examples/ShapeEditor.js
+++ examples/ShapeEditor.js
@@ -60,14 +60,7 @@ requirejs(['./WorldWindShim',
 
         var highlightAttributes = new WorldWind.ShapeAttributes(attributes);
         highlightAttributes.outlineColor = WorldWind.Color.RED;
-
-        var polylineAttributes = new WorldWind.ShapeAttributes(null);
-        polylineAttributes.outlineColor = WorldWind.Color.BLACK;
-        polylineAttributes.interiorColor = new WorldWind.Color(0.8, 0.9, 0.9, 1.0);
-        polylineAttributes.outlineWidth = 5;
-
-        var polylineHighlightAttributes = new WorldWind.ShapeAttributes(polylineAttributes);
-        polylineHighlightAttributes.outlineColor = WorldWind.Color.RED;
+        highlightAttributes.outlineWidth = 5;
 
         var circleShape = new WorldWind.SurfaceCircle(new WorldWind.Location(35, -110), 200e3, attributes);
         circleShape.highlightAttributes = highlightAttributes;
@@ -113,8 +106,8 @@ requirejs(['./WorldWindShim',
         polylineBoundaries.push(new WorldWind.Location(40, -115));
         polylineBoundaries.push(new WorldWind.Location(43, -110));
         polylineBoundaries.push(new WorldWind.Location(50, -120));
-        var polylineShape = new WorldWind.SurfacePolyline(polylineBoundaries, polylineAttributes);
-        polylineShape.highlightAttributes = polylineHighlightAttributes;
+        var polylineShape = new WorldWind.SurfacePolyline(polylineBoundaries, attributes);
+        polylineShape.highlightAttributes = highlightAttributes;
         shapesLayer.addRenderable(polylineShape);
 
         var rectangleShape = new WorldWind.SurfaceRectangle(new WorldWind.Location(33, -105), 300e3, 200e3, 70, attributes);
