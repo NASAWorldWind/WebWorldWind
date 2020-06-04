4d4b35d6f7df648ca2aa48b86b360bc773318925
Miguel Del Castillo <migueldelcastillo@live.com>
2020-06-04 10:09:34 -0500

    Added validation for when the KML file doesn't specify altitudeMode  - Resolves issue of KmlPolygon not creating SurfacePolygon when KML file does not specify altitudeMode.
     - As per KML's 2.2 specification, if altitudeMode is not specified, it should be interpreted as CLAMP_TO_GROUND. This commit ensures this behavior.
    
     - Added method for validation of altitudeMode existence in KML file, imitating WWJ's KMLPlacemark behavior.

--- src/formats/kml/geom/KmlPolygon.js
+++ src/formats/kml/geom/KmlPolygon.js
@@ -140,7 +140,8 @@ define([
      * @param styles.highlight {KmlStyle} Style to apply when item is highlighted. Currently ignored.
      */
     KmlPolygon.prototype.createPolygon = function(styles, fileCache) {
-        if(this.kmlAltitudeMode === WorldWind.CLAMP_TO_GROUND ||
+        if(!this.isValidAltitudeMode(this.kmlAltitudeMode) ||
+            this.kmlAltitudeMode === WorldWind.CLAMP_TO_GROUND ||
             (this.kmlInnerBoundary && this.kmlInnerBoundary.kmlAltitudeMode === WorldWind.CLAMP_TO_GROUND) ||
             (this.kmlOuterBoundary && this.kmlOuterBoundary.kmlAltitudeMode === WorldWind.CLAMP_TO_GROUND)) {
             this._renderable = new SurfacePolygon(this.prepareLocations(), this.prepareAttributes(styles.normal, fileCache));
@@ -204,6 +205,16 @@ define([
             locations = this.kmlOuterBoundary.kmlPositions;
         }
         return locations;
+    };
+
+    /**
+     * @inheritDoc
+     */
+    KmlPolygon.prototype.isValidAltitudeMode = function (altMode) {
+        return "clampToGround" === altMode
+            || "relativeToGround" === altMode
+            || "absolute" === altMode;
+
     };
 
     /**
