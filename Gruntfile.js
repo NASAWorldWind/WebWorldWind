0ad066a684f58fd3fdc68e14850a7b20b30726c0
markpet49 <markpet@yahoo.com>
2022-04-13 15:41:18 -0500

    Version change and package build restoration

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,5 +133,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-zip');
 
-    grunt.registerTask('default', ['requirejs']);
+    grunt.registerTask('default', ['clean', 'karma', 'jsdoc', 'requirejs', 'copy', 'zip']);
 };
