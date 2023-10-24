a5652efd30d74a9d55be82769f1783a652739e45
Beak-man <migueldelcastillo@live.com>
2023-10-24 16:20:46 -0500

    Added version consistency check script

--- src/WorldWind.js
+++ src/WorldWind.js
@@ -592,10 +592,9 @@ define([ // PLEASE KEEP ALL THIS IN ALPHABETICAL ORDER BY MODULE NAME (not direc
         var WorldWind = {
             /**
              * The WorldWind version number.
-             * @default "0.9.0"
              * @constant
              */
-            VERSION: "0.9.0",
+            VERSION: "0.11.0",
 
             // PLEASE KEEP THE ENTRIES BELOW IN ALPHABETICAL ORDER
             /**
